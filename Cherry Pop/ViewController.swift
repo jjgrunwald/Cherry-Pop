0ebe39f4573207240f4fc4e6a87d78c96d7f2412
JJ Grunwald <jjgrunwald@gmail.com>
2023-10-02 16:49:42 -0600

    All of the logic of the form is now working correctly. The only thing I'm going to have to do is add a string and counter.

--- Cherry Pop/ViewController.swift
+++ Cherry Pop/ViewController.swift
@@ -8,10 +8,11 @@
 import UIKit
 
 
-var cherryAmount = 1;
+var cherryAmount = 3;
 
 var cherryArray =
-[UIImage(imageLiteralResourceName: "oneCherry"),
+[UIImage(imageLiteralResourceName: "zeroCherry"),
+ UIImage(imageLiteralResourceName: "oneCherry"),
  UIImage(imageLiteralResourceName: "twoCherry"),
  UIImage(imageLiteralResourceName: "threeCherry")]
 
@@ -24,23 +25,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var cherryImageView: UIImageView!
     
     // Start a timer to increment cherryAmount until it reaches 3
-    func startTimer() { Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(incrementCherry), userInfo: nil, repeats: true)
+    func startTimer() { Timer.scheduledTimer(timeInterval: 4.5, target: self, selector: #selector(incrementCherry), userInfo: nil, repeats: true)
     }
     
     @objc func incrementCherry() {
         print("Timer is working.")
         
         if cherryAmount == 0
-        { cherryAmount += 1;
-            cherryImageView.isHidden = true}
-        
+        { cherryAmount = 1;
+            print("Cherry = 0")}
+        else
         if cherryAmount == 1
-        { cherryAmount += 1;
-            }
-        
+        { cherryAmount = 2;
+            print("Cherry = 1")}
+        else
         if cherryAmount == 2
-        { cherryAmount += 1;
-        }
+        { cherryAmount = 3;
+            print("Cherry = 2")}
+        else
+        if cherryAmount == 3
+        { cherryAmount = 3;
+            print("Cherry = 3")}
         
         cherryImageView.image =
         cherryArray[cherryAmount]
@@ -55,13 +60,27 @@ class ViewController: UIViewController {
     
     @IBAction func pressPopButton(_ sender: UIButton) {
         
+               
+        if cherryAmount == 3
+        { cherryAmount = 2;
+            print("Cherry = 2")}
+        else
+        if cherryAmount == 2
+        { cherryAmount = 1;
+            print("Cherry = 1")}
+        else
+        if cherryAmount == 1
+        { cherryAmount = 0;
+            print("Cherry = 0")}
+        else
+        if cherryAmount == 3
+        { cherryAmount = 3;
+            print("Cherry = 3")}
+        
         cherryImageView.image =
         cherryArray[cherryAmount]
-        
-        if (cherryAmount >= 1){
-            cherryAmount = cherryAmount - 1;
         }
     }
-}
+
     
 

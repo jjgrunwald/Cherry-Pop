2b2945c373afe573d95bc82fbc00fd1081044400
JJ Grunwald <jjgrunwald@gmail.com>
2023-10-02 14:24:21 -0600

    Working on timer to make cherries re-appear.

--- Cherry Pop/ViewController.swift
+++ Cherry Pop/ViewController.swift
@@ -8,24 +8,58 @@
 import UIKit
 
 
-class ViewController: UIViewController {
+var cherryAmount = 1;
 
+var timer: Timer?
+
+class ViewController: UIViewController {
+    
     
     @IBOutlet weak var cherryImageView: UIImageView!
     
-    var cherryAmount = 1;
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    
-        
+    // Start a timer to increment cherryAmount until it reaches 3
+    func startTimer() { Timer.scheduledTimer(timeInterval: 1.0, target: ViewController.self, selector: #selector(incrementCherry), userInfo: nil, repeats: true)
     }
-    @IBAction func pressPopButton(_ sender: UIButton) {
-         
+    
+    @objc func incrementCherry() {
+        print("Timer is working.")
+        
         cherryImageView.image =
         [UIImage(imageLiteralResourceName: "oneCherry"),
-       UIImage(imageLiteralResourceName: "twoCherry"),
-         UIImage(imageLiteralResourceName:"threeCherry")][cherryAmount]
+         UIImage(imageLiteralResourceName: "twoCherry"),
+         UIImage(imageLiteralResourceName: "threeCherry")][cherryAmount]
+        
+        if cherryAmount == 0
+        { cherryAmount += 1;
+            cherryImageView.isHidden = true}
+        
+        if cherryAmount == 1
+        { cherryAmount += 1;
+            }
+        
+        if cherryAmount == 2
+        { cherryAmount += 1;
+        }
+        
+      
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+    
+    @IBAction func pressPopButton(_ sender: UIButton) {
+        
+        cherryImageView.image =
+        [UIImage(imageLiteralResourceName: "oneCherry"),
+         UIImage(imageLiteralResourceName: "twoCherry"),
+         UIImage(imageLiteralResourceName: "threeCherry")][cherryAmount]
+        
+        cherryAmount = cherryAmount - 1;
+        
     }
 }
+    
 

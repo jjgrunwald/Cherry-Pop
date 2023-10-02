10469bfae6ac5770dc09152beb42a9d9af2b5e4f
JJ Grunwald <jjgrunwald@gmail.com>
2023-10-02 12:06:36 -0600

    So far I have the images loaded into an array, some graphics, and a button on the main page.

--- Cherry Pop/ViewController.swift
+++ Cherry Pop/ViewController.swift
@@ -7,13 +7,25 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var cherryImageView: UIImageView!
+    
+    var cherryAmount = 1;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        
     }
-
-
+    @IBAction func pressPopButton(_ sender: UIButton) {
+         
+        cherryImageView.image =
+        [UIImage(imageLiteralResourceName: "oneCherry"),
+       UIImage(imageLiteralResourceName: "twoCherry"),
+         UIImage(imageLiteralResourceName:"threeCherry")][cherryAmount]
+    }
 }
 

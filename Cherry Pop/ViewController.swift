3167bab52ec42ef2a1d885504b18ba5040651ada
JJ Grunwald <jjgrunwald@gmail.com>
2023-10-02 22:08:40 -0600

    Finished my app, should probably add some sort of sound and vibration if possible.

--- Cherry Pop/ViewController.swift
+++ Cherry Pop/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 var cherryAmount = 3;
 
+var playerScore = 0;
+
 var cherryArray =
 [UIImage(imageLiteralResourceName: "zeroCherry"),
  UIImage(imageLiteralResourceName: "oneCherry"),
@@ -21,30 +23,40 @@ var timer: Timer?
 
 class ViewController: UIViewController {
     
-    
     @IBOutlet weak var cherryImageView: UIImageView!
     
-    // Start a timer to increment cherryAmount until it reaches 3
+    @IBOutlet weak var scoreUILabel: UILabel!
+    
     func startTimer() { Timer.scheduledTimer(timeInterval: 4.5, target: self, selector: #selector(incrementCherry), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateScoreLabel()
+    {
+        scoreUILabel.text = String(playerScore)
+        
     }
     
     @objc func incrementCherry() {
         print("Timer is working.")
-        
+
         if cherryAmount == 0
         { cherryAmount = 1;
+            updateScoreLabel();
             print("Cherry = 0")}
         else
         if cherryAmount == 1
         { cherryAmount = 2;
+            updateScoreLabel();
             print("Cherry = 1")}
         else
         if cherryAmount == 2
         { cherryAmount = 3;
+            updateScoreLabel();
             print("Cherry = 2")}
         else
         if cherryAmount == 3
         { cherryAmount = 3;
+            updateScoreLabel();
             print("Cherry = 3")}
         
         cherryImageView.image =
@@ -55,32 +67,40 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         startTimer();
+
     }
     
     
     @IBAction func pressPopButton(_ sender: UIButton) {
-        
+        updateScoreLabel()
                
         if cherryAmount == 3
         { cherryAmount = 2;
-            print("Cherry = 2")}
+            playerScore += 1;
+            updateScoreLabel();
+        }
         else
         if cherryAmount == 2
         { cherryAmount = 1;
-            print("Cherry = 1")}
+            playerScore += 1;
+            updateScoreLabel();}
         else
         if cherryAmount == 1
         { cherryAmount = 0;
-            print("Cherry = 0")}
+            playerScore += 1;
+            updateScoreLabel();}
         else
         if cherryAmount == 3
         { cherryAmount = 3;
-            print("Cherry = 3")}
+            playerScore += 1;
+            updateScoreLabel();}
         
         cherryImageView.image =
         cherryArray[cherryAmount]
         }
-    }
+   
+
+}
 
     
 
